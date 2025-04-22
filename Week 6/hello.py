0acff3189071bcb7b57ad37055c654882d532d3b
Mayura <mayurakrishna.23@gmail.com>
2025-04-22 17:24:40 +0530

    Add hello.py

---
+++ Week 6/hello.py
@@ -0,0 +1,12 @@
+# Prompt the user for input and greet with a Hello.
+def greet():
+    name = input("What is your name? ")
+    print(f"Hello, {name}")
+
+
+def main():
+    greet()
+
+
+if __name__ == "__main__":
+    main()
